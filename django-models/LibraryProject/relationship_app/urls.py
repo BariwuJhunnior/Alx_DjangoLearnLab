89950cd23eaba029d8a810c9fe19a1eeeaa65467
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-09 16:36:23 +0000

    updates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.list_books, name='list_books')
-]
-
-from django.urls import path
-from . import views
-
 app_name = 'relationship_app'
 
 urlpatterns = [
+    # Root listing (optional)
+    path('', views.list_books, name='list_books'),
+
     # Function-based view for listing all books
     path('books/', views.list_books, name='book-list'),
     
-    # Class-based view for library details
-    path('library/', views.list_books, name='list_books'),
+    # Class-based view for library details (expects primary key)
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,66 +3,32 @@ from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Book, Library
 
-# Create your views here.
-""" def list_books(request):
-  books = Book.objects.all()
 
-  books_list = []
-  for book in books:
-    book_info = f"Title: {book.title} - Author: {book.author.name}"
-
-    books_list.append(book_info)
-
-  response_text = "\n".join(books_list)
-
-  if not books_list:
-    response_text = "No books found in the database!"
-
-  return HttpResponse(response_text, content_type='text/plain')
-
-class LibraryDetailedView(DetailView):
-    
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add books to context - use self.get_object() instead of self.object
-        library = self.get_object()  # type: ignore[union-attr]
-        context['books'] = library.books.all()  # type: ignore[union-attr]
-        return context
-    
- """
-
-
-from django.shortcuts import render
-from django.views.generic import DetailView, ListView
-from .models import Book, Library
 
 def list_books(request):
-    """
-    A function-based view that lists all books and their authors
-    """
-    books = Book.objects.all().select_related('author')
-    context = {
-        'books': books,
-        'title': 'All Books'
-    }
-    return render(request, 'relationship_app/list_books.html', context)
+  """
+  A function-based view that lists all books and their authors
+  """
+  books = Book.objects.all().select_related('author')
+  context = {
+    'books': books,
+    'title': 'All Books'
+  }
+  return render(request, 'relationship_app/list_books.html', context)
+
 
 class LibraryDetailView(DetailView):
-    """
-    A class-based view that displays details for a specific library
-    including all books available in that library
-    """
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+  """
+  A class-based view that displays details for a specific library
+  including all books available in that library
+  """
+  model = Library
+  template_name = 'relationship_app/library_detail.html'
+  context_object_name = 'library'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        library = self.get_object()
-        context['books'] = library.books.all()
-        return context
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    library = self.get_object()
+    # Use select_related on the books' authors for efficiency
+    context['books'] = library.books.all().select_related('author')
+    return context
