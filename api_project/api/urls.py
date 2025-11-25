619fb6db86c8e7db00affa1101cfa061b7ef578d
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-25 14:23:45 +0000

    Updated 'views.py' with BookViewSet that handles all CRUD operations. Included the configuration using DefaultRouter to route requests to the BookViewSet in the 'url.py'

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import include,path
 from .views import BookList
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"Book", BookViewSet)
 
 
 urlpatterns = [
+  #Route for the BookList view (ListAPIView)
     path("books/", BookList.as_view(), name="book-list"),
+    #Include the router URLs for BookViewSet (all CRUD operations)
+    path('', include(router.urls))
 ]
