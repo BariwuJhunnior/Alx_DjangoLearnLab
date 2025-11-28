0e76ffa89e581fd83b15e2a0c5b7d27c72fa96c7
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-28 23:49:23 +0000

    imported Django.permissions(IsAuthenticated, IsAuthenticatedOrReadOnly separately

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import  IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from .models import Book, Author
 from .serializers import BookSerializer
