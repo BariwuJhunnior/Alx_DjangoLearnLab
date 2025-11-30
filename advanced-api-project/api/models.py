7c7a16fe4cb83adf389efa309ae559b707a7d0cf
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-30 12:23:23 +0000

    Wrote Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,3 +12,8 @@ class Book(models.Model):
   title = models.CharField(max_length=200)
   publication_year = models.DateField(null=True, blank=True)
   author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
