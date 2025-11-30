ef4a083ee535f3ad2339895a42fa687d9755678a
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-11-30 12:27:06 +0000

    imported 'force_authenticate" from 'rest_framework'

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, force_authenticate, APIRequestFactory, APIClient
 from django.contrib.auth.models import User
 from .models import Book
 
