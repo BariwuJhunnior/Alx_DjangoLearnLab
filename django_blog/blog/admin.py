687652d5a614be790a9f10c45a077bd6a1170556
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-12-07 17:05:04 +0000

    fixed Post and Profile models registration in the admin.py file

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Profile, Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Post model.
+    """
+    list_display = ['title', 'author', 'published_date', 'is_published']
+    list_filter = ['is_published', 'published_date', 'author']
+    search_fields = ['title', 'content', 'author__username']
+    date_hierarchy = 'published_date'
+    ordering = ['-published_date']
+    readonly_fields = ['published_date', 'updated_date']
+    
+    fieldsets = (
+        ('Post Information', {
+            'fields': ('title', 'content', 'author')
+        }),
+        ('Publication Details', {
+            'fields': ('is_published', 'published_date', 'updated_date'),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Profile model.
+    """
+    list_display = ['user', 'location', 'website', 'birth_date']
+    list_filter = ['location', 'birth_date']
+    search_fields = ['user__username', 'bio', 'location', 'website']
+    readonly_fields = ['user']
