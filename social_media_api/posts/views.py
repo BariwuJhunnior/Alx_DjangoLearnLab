bad30b8c28526e368de79c3e5b9694a4f1a1416e
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-12-15 05:15:43 +0000

    Added missing 'Response' import

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .pagination import StandardResultsSetPagination
